20edeffdad557349de1408bdbb5a68c95140e06b
thcvors <thcvors@yahoo.com>
2024-12-01 01:52:39 +0900

    Initial Commit

---
+++ ModIPA/ModIPAApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModIPAApp.swift
+//  ModIPA
+//
+//  Created by CVPRO on 12/1/24.
+//
+
+import SwiftUI
+
+@main
+struct ModIPAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ModIPATests/ModIPATests.swift
@@ -0,0 +1,17 @@
+//
+//  ModIPATests.swift
+//  ModIPATests
+//
+//  Created by CVPRO on 12/1/24.
+//
+
+import Testing
+@testable import ModIPA
+
+struct ModIPATests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
